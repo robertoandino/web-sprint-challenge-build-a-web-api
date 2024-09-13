97deb8bd7a56044d948ed9a7e654a7b909d389b6
Roberto Andino <andinorobert518@yahoo.com>
2024-09-13 12:19:33 -0400

    Done with project endpoints

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,8 +1,8 @@
 // add middlewares here related to projects
 const Project = require('./projects-model')
 
+//logger to track requests on the server
 function logger(req, res, next) {
-    // DO YOUR MAGIC
     const timestamp = new Date().toLocaleString()
     const method = req.method
     const url = req.originalUrl
@@ -11,7 +11,7 @@ function logger(req, res, next) {
     next()
 }
 
-
+//validates user id
 async function validateUserId(req, res, next) {
     try{
         const user = await Project.get(req.params.id)
@@ -28,7 +28,25 @@ async function validateUserId(req, res, next) {
     }
 }
 
+//validates project, name and description are required
+function validateProject(req, res, next) {
+
+    const { name, description, completed} = req.body
+
+    if(!name || !name.trim() || !description || !description.trim() || completed === undefined){
+        res.status(400).json({
+            message: 'missing required name, description, or completed field',
+        })
+    } else{
+        req.name = name.trim()
+        req.description = description.trim()
+        next()
+    }
+
+}
+
 module.exports = {
     logger,
     validateUserId,
+    validateProject,
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,6 +1,6 @@
 // Write your "projects" router here!
 const express = require('express');
-const { validateUserId, } = require('./projects-middleware')
+const { validateUserId, validateProject, } = require('./projects-middleware')
 
 const Project = require('./projects-model');
 
@@ -22,6 +22,54 @@ router.get('/:id', validateUserId, (req, res) => {
     res.json(req.user)
 })
 
+//Returns a newly created project as the body of the response
+//If the request body is missing any of the required fields it responds with a status code 400
+router.post('/', validateProject, (req, res, next) => {
+    Project.insert({ name: req.name, description: req.description, completed: req.body.completed })
+        .then(newProject => {
+            res.status(201).json(newProject)
+        })
+        .catch(next)
+})
+
+//Returns an updated project as the body of the response
+//If there is no project with the given id it responds with a status code 404
+//If the requrest body is missing any of the required fields it responds with a status code 400
+router.put('/:id', validateUserId, validateProject, (req, res, next) => {
+    
+    Project.update(req.params.id, { 
+        name: req.name, 
+        description: req.description, 
+        completed: req.body.completed
+    })
+        .then(updatedProject => {
+            res.json(updatedProject)
+        })
+        .catch(next)
+})
+
+
+//Returns no response body
+//If there is no project with the given id, it responds with a status code 400
+router.delete('/:id', validateUserId, async (req, res, next) => {
+    try{
+        await Project.remove(req.params.id)
+        res.json(req.project)
+    } catch (err) {
+        next (err)
+    }
+})
+
+//Returns an array of actions (coult be empty) belonging to a project with the given id
+//If there is no project with the given id, it responds with a status code 404
+router.get('/:id/actions', validateUserId, async (req, res, next) => {
+    try{
+        const result = await Project.getProjectActions(req.params.id)
+        res.json(result)
+    } catch (err) {
+        next (err)
+    }
+})
 
 //Middleware to handle errors
 router.use((err, req, res, next) => {
